1f0de89012cc33ce5193aab7a5da2431665e5912
Greq <papaguye200@gmail.com>
2020-10-12 20:58:34 +0300

    Допрашиваем людей с циклами

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const numberofFilms = +prompt ('Сколько фильмов вы уже посмотрели?', '');
+const numberofFilms = prompt ('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberofFilms,
@@ -9,13 +9,29 @@ const personalMovieDB = {
 
 };
 
-const a = prompt('One of the last film u see?', ''),
-      b = prompt('How u will rate it?', ''),
-      c = prompt('One of the last film u see?', ''),    
-      d = prompt('How u will rate it?', '');
-      
- 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i =0; i < 2; i++) {
+    const a = prompt('One of the last film u see?', ''),
+          b = prompt('How u will rate it?', '');
+    
+          if (a != null && b !=null && a !='' && b != '' && a.length < 50 ) {
+            personalMovieDB.movies[a] = b;
+            console.log('Krasava');  
+          } else {
+              console.log('dyrak');
+              i--;
+          }
 
-console.log(personalMovieDB);      
+    personalMovieDB.movies[a] = b;      
+}
+ 
+if (personalMovieDB.count < 10) {
+    console.log("Просмотренно довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB < 30){
+    console.log("Достаточно много фильмов просмотренно");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Ты гуру фильмов");
+} else {
+    console.log("Ошибочка вышла");
+}
+
+console.log(personalMovieDB);  
